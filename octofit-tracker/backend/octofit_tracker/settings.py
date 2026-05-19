7e18c1f4bb85a1d42758551dc0683ad592ed15d6
Ryan Crell <113032641+rwcrell-kr@users.noreply.github.com>
2026-05-19 14:38:36 +0000

    Update ALLOWED_HOSTS and API URLs for Codespace compatibility and HTTPS support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-d$+p=mq$o0%iwwgo)4!ou@o#@g@&lnm=p7e!f&zk_pr^7jg@ii
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
